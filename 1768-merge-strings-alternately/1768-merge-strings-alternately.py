b3f4342475c2e8e3e2940882df2995ecc8d6a292
Atakan Taş <143446361+AtakaanShiva@users.noreply.github.com>
2026-05-10 18:44:40 +0300

    Time: 20 ms (28.26%), Space: 12.2 MB (91.15%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -0,0 +1,19 @@
+
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+
+        result = ""
+
+        for i in range(min(len(word1), len(word2))):
+            result += word1[i]
+            result += word2[i]
+
+        result += word1[i + 1:]
+        result += word2[i + 1:]
+
+        return result
+
+
+sol = Solution()
+
+print(sol.mergeAlternately("abc", "pgrs"))
